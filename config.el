f0c62fe4350063c7dff45d83792c160ec094655d
Galen Menzel <galen.menzel@utexas.edu>
2019-07-11 22:36:18 -0700

    Add some packages. org-brain keybindings. Tidy up.

--- config.el
+++ config.el
@@ -2,29 +2,52 @@
 
 ;; Place your private configuration here
 
-;; Unicode font configuration
+;; Packages
+;; ========
 
+;; PUT THIS FIRST IN THE CONFIG!
+;; This needs to load before any other packages,
+;; which might do things with unicode fonts that will make emacs hang
+;; if this package isn't loaded and set up.
 (def-package! unicode-fonts
               :config
               (unicode-fonts-setup))
 
-;; default GUI font
+;; If we don't declare this, flycheck won't find flycheck-ledger.
+;; Not sure why this or an explicit ~require~ is needed
+(def-package! flycheck-ledger)
 
-(set-default-font "IBM Plex Mono")
-(set-face-attribute 'default nil :height 140)
+(def-package! ledger-mode)
 
-;; evil configuration
+(def-package! org-ref
+  :config
+  (setq reftex-default-bibliography '("~/Dropbox/bibliography/references.bib"))
+  (setq org-ref-bibliography-notes "~/Dropbox/bibliography/notes.org"
+        org-ref-default-bibliography '("~/Dropbox/bibliography/references.bib")
+        org-ref-pdf-directory "~/Dropbox/bibliography/bibtex-pdfs/"))
 
-(setq evil-cross-lines t)
+;; Doom package customization
+;; ==========================
 
-;; org-mode setup
+;; Disable snipe's default =s= keybinding
+(after! evil-snipe (evil-snipe-mode -1))
 
-;; Setting this via custom messes up org-brain.
-;; Evidently custom is called after this config.el is eval'ed.
-(setq org-directory "~/Dropbox/docs/archive/2019/drafts")
-(setq org-id-link-to-org-use-id t)
+;; Don't consider the org-brain buffer to be a popup
+(set-popup-rule! "^\\*org-brain" :ignore t)
+
+(after! org
+  ;; Doom restricts tab cycling to only cycle the current subtree.
+  ;; This restores default behavior.
+  (remove-hook 'org-tab-first-hook #'+org|cycle-only-current-subtree)
+  (setq org-directory "~/Dropbox/docs/archive/2019/drafts")
+  (setq org-id-link-to-org-use-id t)
+  (setq org-cycle-emulate-tab nil))
+
+(after! evil
+  (setq evil-cross-lines t))
 
 ;; org-brain setup
+;; ===============
 
 ;; For some reason flycheck gets confused
 ;; if I don't specify the .el file here,
@@ -33,37 +56,69 @@
 ;; so I'm listing the file here to placate it.
 (add-to-list 'load-path "~/gits/org-brain")
 (require 'org-brain)
-(evil-set-initial-state 'org-brain-visualize-mode 'emacs)
+;; (evil-set-initial-state 'org-brain-visualize-mode 'emacs)
+
+;; General emacs configuration
+;; ===========================
+
+(set-default-font "IBM Plex Mono")
+(set-face-attribute 'default nil :height 140)
 
 ;; Key bindings
+;; ============
 
-(map! :leader
+(map! (:leader
+        "br" #'revert-buffer
+        "bs" #'org-tree-to-indirect-buffer
+        "er" #'eval-region
+        "ir" #'indent-region
+        "ob" #'org-brain-visualize
+        "x"  #'counsel-M-x)
 
-      (:prefix "b"
-        :desc "Open subtree in new buffer" "s"    (lambda ()
-                                                    (interactive)
-                                                    (clone-indirect-buffer nil 1)
-                                                    (org-narrow-to-subtree))) ;;  #'eval-region)
+      (:mode org-mode
+        :mnv "[o" #'org-previous-visible-heading
+        :mnv "]o" #'org-next-visible-heading
+        :mnv "[*" #'outline-up-heading
+        :mni [(shift meta return)] 'org-insert-subheading
 
-      (:prefix ("e" . "eval")
-        :desc "Eval region" "r" #'eval-region)
+        "s-0" (lambda () (interactive) (show-all))
+        "s-1" (lambda () (interactive) (org-global-cycle 1))
+        "s-2" (lambda () (interactive) (org-global-cycle 2))
+        "s-3" (lambda () (interactive) (org-global-cycle 3))
+        "s-4" (lambda () (interactive) (org-global-cycle 4))
+        "s-5" (lambda () (interactive) (org-global-cycle 5))
+        "s-6" (lambda () (interactive) (org-global-cycle 6))
+        "s-7" (lambda () (interactive) (org-global-cycle 7))
+        "s-8" (lambda () (interactive) (org-global-cycle 8))
+        "s-9" (lambda () (interactive) (org-global-cycle 9)))
 
-      (:prefix "i"
-        :desc "Indent region" "r" #'indent-region)
+      (:mode ledger-mode
+        "s-0"     (lambda () (interactive) (set-selective-display (* tab-width 0)))
+        "s-1"     (lambda () (interactive) (set-selective-display (* tab-width 1)))
+        "s-2"     (lambda () (interactive) (set-selective-display (* tab-width 2)))
+        "s-3"     (lambda () (interactive) (set-selective-display (* tab-width 3)))
+        "s-4"     (lambda () (interactive) (set-selective-display (* tab-width 4)))
+        "s-5"     (lambda () (interactive) (set-selective-display (* tab-width 5)))
+        "s-6"     (lambda () (interactive) (set-selective-display (* tab-width 6)))
+        "s-7"     (lambda () (interactive) (set-selective-display (* tab-width 7)))
+        "s-8"     (lambda () (interactive) (set-selective-display (* tab-width 8)))
+        "s-9"     (lambda () (interactive) (set-selective-display (* tab-width 9))))
 
-      (:prefix "n"
-        :desc "org-brain-visualize" "b" #'org-brain-visualize)
-
-      :desc "counsel-M-x"           "x" #'counsel-M-x
-
-      )
-
-(define-key org-brain-visualize-mode-map "/" 'counsel-brain)
-
-;; Doom customization
-
-;; Disable snipe's default =s= keybinding
-(after! evil-snipe (evil-snipe-mode -1))
-
-;; Don't consider the org-brain buffer to be a popup
-(set-popup-rule! "^\\*org-brain" :ignore t)
+      (:mode org-brain-visualize-mode
+        :n "/"   #'counsel-brain
+        :n "TAB" #'forward-button
+        :n "C-j" #'forward-button
+        :n "C-k" #'backward-button
+        :n "o"   #'org-brain-goto-current
+        :n "gp"  #'org-brain-goto-parent
+        :n "gc"  #'org-brain-goto-child
+        :n "gr"  #'revert-buffer
+        :n "p"   #'org-brain-add-parent
+        :n "P"   #'org-brain-remove-parent
+        :n "c"   #'org-brain-add-child
+        :n "C"   #'org-brain-remove-child
+        :n "f"   #'org-brain-add-friendship
+        :n "F"   #'org-brain-remove-friendship
+        :n ";"   #'org-brain-select
+        :n ":"   #'org-brain-clear-selected
+        :n "n"   #'org-brain-pin))
--- packages.el
+++ packages.el
@@ -6,4 +6,7 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
+(package! flycheck-ledger)
+(package! ledger-mode)
+(package! org-ref)
 (package! unicode-fonts)
